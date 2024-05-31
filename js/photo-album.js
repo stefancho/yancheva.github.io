386f27666fb4f9e23c9c1eb94c076fd5be21dd5c
stefancho <stefanstoqnov@gmail.com>
2024-05-31 15:03:59 +0300

    Add arrows in image gallery

--- js/photo-album.js
+++ js/photo-album.js
@@ -1,69 +1,118 @@
 function createImage(src) {
-    const image = document.createElement('img');
-    image.src = src;
-    return image;
+  const image = document.createElement('img');
+  image.src = src;
+  return image;
+}
+
+function createHyperLink(href) {
+  const a = document.createElement('a');
+  a.href = href;
+  return a;
+}
+
+function createArrow(nextArrow){
+  const span = document.createElement('span');
+  span.classList.add('material-symbols-outlined');
+  span.classList.add('white-text');
+  span.classList.add('pointer');
+
+  textContent = null;
+  if(nextArrow) {
+    span.textContent = "arrow_forward";
   }
-  
-  function onThumbnailClick(event) {
-    currentIndex = event.currentTarget.dataset.index;
-    const image = createImage(event.currentTarget.src);
-    modalView.appendChild(image);
-  
-    document.body.classList.add('no-scroll');
-    modalView.style.top = (window.scrollY || window.pageYOffSet) + 'px';
-    modalView.classList.remove('hidden');
-  
-    document.addEventListener('keydown', nextPhoto);
+  else {
+    span.textContent = "arrow_back";
   }
-  
-  function onModalClick() {
+  return span;
+}
+// Appends image with imgSrc along with forward and backward arrows
+function appendImage(container, imgSrc) {
+
+  const image = createImage(imgSrc);
+  container.appendChild(prevArrow)
+  container.appendChild(image);
+  container.appendChild(nextArrow);
+}
+
+function onThumbnailClick(event) {
+  event.preventDefault();
+  currentIndex = event.currentTarget.dataset.index;
+  appendImage(modalView, event.currentTarget.src);
+
+  document.body.classList.add('no-scroll');
+  modalView.style.top = (window.scrollY || window.pageYOffSet) + 'px';
+  modalView.classList.remove('hidden');
+
+  document.addEventListener('keydown', nextPhoto);
+}
+
+function onModalClick() {
+  hideModal();
+}
+
+function onNextImgClick(event) {
+  nextPhoto({key:"ArrowRight"})
+  event.stopPropagation();
+}
+
+function onPrevImgClick(event) {
+  nextPhoto({key:"ArrowLeft"})
+  event.stopPropagation();
+}
+
+
+function hideModal() {
+  document.body.classList.remove('no-scroll');
+  modalView.classList.add('hidden');
+  modalView.innerHTML = '';
+  document.removeEventListener('keydown', nextPhoto);
+}
+
+function nextPhoto(event) {
+  if (event.key === 'Escape') {
     hideModal();
+    return;
   }
-  
-  function hideModal() {
-    document.body.classList.remove('no-scroll');
-    modalView.classList.add('hidden');
-    modalView.innerHTML = '';
-    document.removeEventListener('keydown', nextPhoto);
+
+  if (event.key !== 'ArrowLeft' && event.key !== 'ArrowRight') {
+    return;
   }
-  
-  function nextPhoto(event) {
-    if (event.key === 'Escape') {
-      hideModal();
-      return;
-    }
-  
-    if (event.key !== 'ArrowLeft' && event.key !== 'ArrowRight') {
-      return;
-    }
-  
-    let nextIndex = currentIndex;
-    if (event.key === 'ArrowLeft') {
-      nextIndex--;
-    } else {
-      nextIndex++;
-    }
-  
-    if (nextIndex < 0 || nextIndex == PHOTO_LIST.length) {
-      return;
-    }
-    const photoSrc = PHOTO_LIST[nextIndex];
-    modalView.innerHTML = '';
-    const image = createImage(photoSrc);
-    modalView.appendChild(image);
-    currentIndex = nextIndex;
+
+  let nextIndex = currentIndex;
+  if (event.key === 'ArrowLeft') {
+    nextIndex--;
+  } else {
+    nextIndex++;
   }
-  
-  let currentIndex = null;
-  const albumView = document.querySelector('#images');
-  for (let i = 0; i < PHOTO_LIST.length; i++) {
-    const photoSrc = PHOTO_LIST[i];
-    const image = createImage(photoSrc);
-    image.dataset.index = i;
-    image.addEventListener('click', onThumbnailClick);
-    albumView.appendChild(image);
+
+  if (nextIndex < 0 || nextIndex == PHOTO_LIST.length) {
+    return;
   }
-  
-  const modalView = document.querySelector('#modal-view');
-  modalView.addEventListener('click', onModalClick);
-  
+  const photoSrc = PHOTO_LIST[nextIndex];
+  modalView.innerHTML = '';
+  appendImage(modalView, photoSrc);
+  currentIndex = nextIndex;
+}
+
+
+
+let currentIndex = null;
+const albumView = document.querySelector('#images');
+for (let i = 0; i < PHOTO_LIST.length; i++) {
+  const photoSrc = PHOTO_LIST[i];
+  const image = createImage(photoSrc);
+  const containerLink = createHyperLink('#');
+  containerLink.appendChild(image);
+  image.dataset.index = i;
+  image.addEventListener('click', onThumbnailClick);
+  albumView.appendChild(containerLink);
+}
+
+const modalView = document.querySelector('#modal-view');
+modalView.addEventListener('click', onModalClick);
+
+const nextArrow = createArrow(true);
+const prevArrow = createArrow(false);
+nextArrow.addEventListener('click', onNextImgClick);
+prevArrow.addEventListener('click', onPrevImgClick);
+
